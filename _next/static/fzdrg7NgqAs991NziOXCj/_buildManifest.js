c7a6ea0523e6a63d5f61b9b4b003734611657814
Rotwang9000 <rotwang@bitx.live>
2023-11-06 12:06:09 +0000

    Updates

--- _next/static/fzdrg7NgqAs991NziOXCj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/466-ee982c76d72f407a.js","static/css/0463640c4945480c.css","static/chunks/pages/index-d7940fe80c32fb7a.js"],"/_error":["static/chunks/pages/_error-a9572f84d60f21da.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
